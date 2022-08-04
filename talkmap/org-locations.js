07e0d0199ba65588cd85fb640356bd3b974cb532
Sid Kerhalkar <66088958+kerhalkarsid@users.noreply.github.com>
2022-08-03 23:30:06 -0400

    changes_bloody_changes

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Los Angeles, CA",
+    34.0536909,
+    -118.242766
   ],
   [
     "London, UK",
     51.5073219,
-    -0.1276473
+    -0.1276474
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Berkeley CA, USA",
+    37.8753497,
+    -122.23963364918777
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Hyderabad, India",
+    17.360589,
+    78.4740613
   ]
 ];
